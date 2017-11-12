fbeec3600a045f7b52038e6ddbc73a27eff294e6
personalgithubzhu <33590816+personalgithubzhu@users.noreply.github.com>
2017-11-12 14:29:16 +0800

    删除Java1.Java
    第一次使用Java文件，练习使用

--- java1.java
+++
@@ -1,8 +0,0 @@
-public class test{
-  public static void main(String[] args){
-      int i=1;
-      int j=2;
-      System.out.println("j除以i的结果是"+j/i);、
-      System.out.println("你是我心爱的姑娘");
-  }  
-}
